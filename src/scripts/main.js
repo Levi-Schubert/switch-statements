4130c9eb43ccc639dc94e27eb27c0d0d32d91e90
Levi Schubert <me@levischubert.com>
2018-04-27 09:15:21 -0500

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+let day = "Monday"
+
+switch (day) {
+	case "Monday":
+		console.log("Happy Monday")
+		break;
+	case "Tuesday":
+		console.log("It's Tuesday. You got this!")
+		break;
+	case "Wednesday":
+		console.log("Hump day already!")
+		break;
+	case "Thursday":
+		console.log("Just one more day 'til the weekend!")
+		break;
+	case "Friday":
+		console.log("Happy Friday!")
+		break;
+	case "Saturday":
+		console.log("Have a wonderful Saturday")
+		break;
+	case "Sunday":
+		console.log("It's Sunday, time to relax!")
+		break;
+	default:
+		console.log("Please specify a day of the week.")
+		break;
+}
